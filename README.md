b6bd5e71094605bea8531d9e52c604a1ec04a557
Bitplane <info@bitplane.infowq>
2017-11-19 07:27:57 +0000

    Commit her everything

---
+++ README.md
@@ -0,0 +1,26 @@
+# unicorn_chan
+Hearted image scavenger from Twitter
+
+## What is this?
+This is a discord bot who collects tweets with images you hearted from accounts you specified, and paste on discord.
+
+## Who's Unicorn Chan?
+She's my waifu.
+She's the cutest girl ever.
+http://blhx.wikia.com/wiki/Unicorn
+
+## How to Use?
+
+1. ***installation***
+  1. install latest Node.js
+  2. clone her
+  3. create database 'unicorn_chan'
+  4. `npm init`, `cp vars.js.sample vars.js` and `vim vars.js (edit as you suite)`
+  5. `node index.js` to wake her up
+
+2. ***bot commands***
+  1. `!watch @xxxxxx` - Tell her who's illustrator you wan to collects. 
+  2. `!unwatch @xxxxxx` - You might know.
+  3. `!list` - She'll give you illustrator list she's watching. 
+  4. `!kawaii` - Kawaii.
+
---
+++ index.js
@@ -0,0 +1,150 @@
+const vars    = require('./vars');
+
+const Discord = require('discord.js');
+let   unicorn = new Discord.Client();
+const Twitter = require('twitter');
+const twitter = new Twitter(vars.twitter);
+const knex    = require('knex')(vars.database);
+
+
+// --------------------------------
+async function getIllustrators() {
+	try {
+		let table = await knex('illustrators');
+		let illustrators = [];
+		Object.keys(table).forEach(line => {
+			illustrators.push(table[line].name);
+		});
+
+		return illustrators;
+
+	} catch(e) {
+		if(createTable())
+			return [];
+
+		console.log('table creation failed');
+		process.exit(1);
+	}
+}
+
+
+async function save(name) {
+	try {
+		let exist = await knex('illustrators').where('name', '=', name);
+
+		if(exist.length == 0) {
+			unicorn.illustrators.push(name);
+			await knex('illustrators').insert({name: name});
+		}
+
+	} catch(e) {
+		console.log('saving failed');
+		process.exit(1);
+	}
+}
+
+
+async function remove(name) {
+	try {
+		let exist = await knex('illustrators').where('name', '=', name);
+
+		if(exist.length != 0) {
+			unicorn.illustrators.splice(unicorn.illustrators.indexOf(name), 1);
+			
+			await knex('illustrators').where({name: name}).del();
+		}
+
+	} catch(e) {
+		console.log('deletion failed');
+		process.exit(1);
+	}
+}
+
+
+async function createTable() {
+	try {
+		await knex.schema.createTable('illustrators', function(table) {
+			table.increments();
+			table.string('name', 32);
+			table.timestamps();
+		});
+
+		return true;
+
+	} catch (e) {
+		return false;
+	}
+}
+
+
+
+// ----------------------------------------------
+unicorn.on('ready', async () => {
+	unicorn.tweetedIds   = [];
+	unicorn.illustrators = await getIllustrators();
+	unicorn.herRoom      = unicorn.channels.get(vars.discord.channelId);
+	
+	setInterval(
+		() => { fav(); },
+		1000 * 60
+	);
+	
+	function fav() {
+		twitter.get('favorites/list', (error, tweets, response) => {
+			tweets.forEach(tweet => {
+				if(-1 === unicorn.tweetedIds.indexOf(tweet.id)) {
+					if(
+						-1 < unicorn.illustrators.indexOf(tweet.user.screen_name.toLowerCase())  &&
+						-1 < tweet.text.indexOf('https://t.co/')
+					) {
+						unicorn.herRoom.send(`https://twitter.com/${tweet.user.screen_name}/status/` + tweet.id_str);
+						unicorn.tweetedIds.push(tweet.id);
+					}
+				}
+			});
+		});
+	}
+});
+	
+
+
+// ----------------------------------------------
+unicorn.on('message', message => {
+	const kawaii  = /^[!]kawaii\s*/;
+	const watch   = /^[!]watch @([0-9a-zA-Z_]+)\s*/;
+	const unwatch = /^[!]unwatch @([0-9a-zA-Z_]+)\s*/;
+	const list    = /^[!]list\s*/;
+
+	if(kawaii.test(message)) {
+		message.reply('Thank you, Oni-chan!');
+	}
+
+	if(watch.test(message)) {
+		let name = watch.exec(message)[1].toLowerCase();
+		save(name);
+		message.reply(`Yes, I'm watching ${name}, Oni-chan!`);
+	}
+
+	if(unwatch.test(message)) {
+		let name = unwatch.exec(message)[1].toLowerCase();
+		remove(name);
+		message.reply(`Yes, I'm unwatching ${name}, Oni-chan!`);
+	}
+
+	if(list.test(message)) {
+		let list = '';
+		
+		unicorn.illustrators.forEach(name => {
+			list += `${name}\n`;
+		});
+
+		message.reply(`I am watching these illustratos\n${list}`);
+	}
+});
+
+
+
+// ----------------------------------------------
+unicorn.login(vars.discord.token);
+
+
